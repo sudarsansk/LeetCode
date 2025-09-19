0839307284c7c8b67aa65af06d9f6d8171def641
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-19 09:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (65.59%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        //Using Index Of
+        //return haystack.indexOf(needle);
+         if(haystack.contains(needle)){
+            if(haystack.length()==1) return 0;
+            int i=0;
+            int j=needle.length();
+            while(j<=haystack.length()){
+                if(haystack.substring(i,j).equals(needle)) return i;
+                i++;j++;
+            }
+        }
+        return -1;
     }
 }
