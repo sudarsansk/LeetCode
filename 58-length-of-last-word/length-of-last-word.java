cbd8db8ea1d6d8f2f6806e1d3a24fce5348a14aa
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-19 09:05:16 +0530

    Time: 1 ms (29.99%) | Memory: 42.3 MB (5.31%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String [] strs = s.split(" ");
+        String [] strs = s.trim().split(" ");
         return strs[strs.length-1].length();
     }
 }
