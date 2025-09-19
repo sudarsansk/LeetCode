7c6641b998edd8b10a1399092444fac41b86020d
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-19 10:00:02 +0530

    Time: 16 ms (40.99%) | Memory: 42.3 MB (86.35%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,36 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        
-        // Two maps to ensure bijective (one-to-one) mapping
-        Map<Character, Character> map1 = new HashMap<>(); // s -> t mapping
-        Map<Character, Character> map2 = new HashMap<>(); // t -> s mapping
-        
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-            
-            // Check if mapping from s to t exists
-            if (map1.containsKey(charS)) {
-                if (map1.get(charS) != charT) {
-                    return false; // Inconsistent mapping
-                }
-            } else {
-                map1.put(charS, charT);
+    if(s.length() != t.length()){
+        return false;
+    }
+    Map<Character,Character> map1 = new HashMap<>();
+    Map<Character,Character> map2 = new HashMap<>();
+    for(int i=0 ; i < s.length(); i++){
+        char first = s.charAt(i);
+        char second = t.charAt(i);
+
+        if(map1.containsKey(first)){
+            if(map1.get(first) != second){
+                return false;
             }
-            
-            // Check if mapping from t to s exists
-            if (map2.containsKey(charT)) {
-                if (map2.get(charT) != charS) {
-                    return false; // Inconsistent reverse mapping
-                }
-            } else {
-                map2.put(charT, charS);
-            }
+        } else{
+            map1.put(first, second);
         }
-        
-        return true;
+
+        if(map2.containsKey(second)){
+            if(map2.get(second) != first){
+                return false;
+            }
+        } else{
+            map2.put(second, first);
+        }
+    }
+    return true;
     }
 }
