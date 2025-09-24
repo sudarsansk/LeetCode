89bb14d5783c1d8a60819335b5dc8c4114669340
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-24 23:12:25 +0530

    Time: 11 ms (16.31%) | Memory: 42 MB (30.65%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,25 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = 0;
+
+        Map<Character, Integer> s_map = new HashMap<>();
+        Map<Character, Integer> t_map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            result ^= c;
+            s_map.put(c, s_map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            if(!s_map.containsKey(c)){
+                return c;
+            }
+            s_map.put(c, s_map.get(c) - 1);
+            if(s_map.get(c) < 0){
+                return c;
+            }
+            
+        }
+        return ' ';
+    }
+}
