e1c8573d15d8b99d025ee1719c9438d397d76319
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-26 11:43:16 +0530

    Time: 1 ms (97.60%) | Memory: 43.4 MB (44.79%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;  // Largest
+        long second = Long.MIN_VALUE; // Second largest  
+        long third = Long.MIN_VALUE;  // Third largest
+        
+        for (int num : nums) {
+            // Skip duplicates
+            if (num == first || num == second || num == third) {
+                continue;
+            }
+            
+            if (num > first) {
+                // Shift values down
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+        
+        // If third maximum doesn't exist, return first maximum
+        if (third == Long.MIN_VALUE) {
+            return (int) first;
+        }
+        
+        return (int) third;
+    }
+}
