b370704523d61fd3862c0b7fe4fd0e654f8cde2e
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-09-26 11:56:20 +0530

    Time: 1 ms (59.86%) | Memory: 41.4 MB (88.89%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,28 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-       if (strs == null || strs.length == 0) {
+    String [] str = strs;
+    if (strs == null || strs.length == 0) {
         return "";
-       }
-         // Use the first string as reference
-    String first = strs[0];
-    int maxCommonLength = first.length();
-    
-    for (int i = 1; i < strs.length; i++) {
-        String current = strs[i];
-        int j = 0;
-        
-        while (j < maxCommonLength && j < current.length() 
-               && first.charAt(j) == current.charAt(j)) {
-            j++;
-        }
-        
-        // Update the maximum common length
-        maxCommonLength = Math.min(maxCommonLength, j);
-        
-        if (maxCommonLength == 0) {
-            return "";
-        }
     }
-    return first.substring(0, maxCommonLength);
+    String first = strs[0];
+    int k = 0;
+    for(; k < first.length() ; k++) {
+		char currentChar = str[0].charAt(k);
+			
+		for(int z =1; z < str.length ; z++) {
+			if(k >= str[z].length() || currentChar != str[z].charAt(k)) {
+				return str[0].substring(0, k);
+			}
+		}
+	}
+
+	return first.substring(0, k);
     }
 }
